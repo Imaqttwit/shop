b6963d22e566c0f263c42adbbd41c5f15f1fc944
Twit Alanis <twitalanis@gmail.com>
2023-05-06 00:49:57 +0800

    first commit

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Support\Str;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create(
+            [
+            'id' => 1,
+            'name' => "Alanis Admin",
+            'email' => "twitalanis@gmail.com",
+            'email_verified_at' => Carbon::now(),
+            'password' => Hash::make('password'),
+            'isAdmin' => 1,
+            'remember_token' => Str::random(10)
+        ]
+    );
+    }
+}
